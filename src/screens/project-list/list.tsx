8d7ee82635747432d036d36bfef1bdc03ca4f6ed
unknown <nanami2962@gmail.com>
2022-04-04 17:07:15 +0800

    style: write css for project list

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,32 +1,62 @@
-import {User} from './search-panel'
-import {Table} from "antd";
+import React from "react";
+import { User } from "./search-panel";
+import { Table } from "antd";
+import dayjs from "dayjs";
 
 interface Project {
-  id: string,
-  name: string,
-  personId: string,
-  pin: boolean,
-  organization: string
+  id: string;
+  name: string;
+  personId: string;
+  pin: boolean;
+  organization: string;
+  created: number;
 }
 
 interface ListProps {
-  list: Project[],
-  users: User[],
+  list: Project[];
+  users: User[];
 }
 
-export const List = ({users, list}: ListProps) => {
+export const List = ({ list, users }: ListProps) => {
   return (
-    <Table pagination={false} columns={[{
-      title: '名称',
-      dataIndex: 'name',
-      sorter: (a, b) => a.name.localeCompare(b.name)
-    }, {
-      title: '负责人',
-      render(value, project) {
-        return <span>
-          {users.find(user => user.id === project.personId)?.name || '未知'}
-        </span>
-      }
-    }]} dataSource={list}> </Table>
-  )
-}
+    <Table
+      rowKey={"id"}
+      pagination={false}
+      columns={[
+        {
+          title: "名称",
+          dataIndex: "name",
+          sorter: (a, b) => a.name.localeCompare(b.name),
+        },
+        {
+          title: "部门",
+          dataIndex: "organization",
+        },
+        {
+          title: "负责人",
+          render(value, project) {
+            return (
+              <span>
+                {users.find((user) => user.id === project.personId)?.name ||
+                  "未知"}
+              </span>
+            );
+          },
+        },
+        {
+          title: "创建时间",
+          render(value, project) {
+            return (
+              <span>
+                {project.created
+                  ? dayjs(project.created).format("YYYY-MM-DD")
+                  : "无"}
+              </span>
+            );
+          },
+        },
+      ]}
+      dataSource={list}
+    />
+  );
+};
